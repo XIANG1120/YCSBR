90a5535841f457ea7832cf0ef4d4350be45adee0
e楼-xlx <1716830949@qq.com>
2023-10-28 16:39:48 +0800

    添加delete_keys_

--- generator/config_impl.cc
+++ generator/config_impl.cc
@@ -392,6 +392,7 @@ Phase WorkloadConfigImpl::GetPhase(const PhaseID phase_id,
     phase.delete_thres = 
         phase_config[kDeleteOpKey][kProportionKey].as<uint32_t>();
     phase.num_deletes = static_cast<size_t>(phase.num_requests * (insert_pct / 100.0));  //计算delete个数
+    phase.num_deletes_left = phase.num_deletes;
     //创建chooser
     phase.delete_chooser =
         CreateChooser(lock, phase_config[kDeleteOpKey][kDistributionKey],
